df3da624f51026b9d678c3e9b5936ea6bc8dbf87
Mark M <chtozamm@gmail.com>
2023-08-05 23:00:06 +0300

    header reposition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,31 @@
-import "@/styles/globals.css";
-import type { Metadata } from "next";
-import Loader from "@/components/Loader";
-import Header from "@/components/Header";
+import "@/styles/globals.css"
+import type { Metadata } from "next"
+import Loader from "@/components/Loader"
 
 export const metadata: Metadata = {
-  title: "Annynotes - The world of fantasy ✨",
-  description: "Share a legend or leave a note for a loved one.",
-  openGraph: {
-    title: "Annynotes - The world of fantasy ✨",
-    description: "Share a legend or leave a note for a loved one.",
-    url: "https://www.annynotes.fun",
-  },
-  twitter: {
-    title: "Annynotes - The world of fantasy ✨",
-    description: "Share a legend or leave a note for a loved one.",
-    card: "summary_large_image",
-  },
-};
+	title: "Annynotes - The world of fantasy ✨",
+	description: "Share a legend or leave a note for a loved one.",
+	openGraph: {
+		title: "Annynotes - The world of fantasy ✨",
+		description: "Share a legend or leave a note for a loved one.",
+		url: "https://www.annynotes.fun",
+	},
+	twitter: {
+		title: "Annynotes - The world of fantasy ✨",
+		description: "Share a legend or leave a note for a loved one.",
+		card: "summary_large_image",
+	},
+}
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <main>
-          <Loader />
-          <Header />
-          {props.children}
-        </main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<main>
+					<Loader />
+					{props.children}
+				</main>
+			</body>
+		</html>
+	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
-import Posts from "@/components/Posts";
+import Posts from "@/components/Posts"
 import {
-  createPost,
-  deletePost,
-  getPosts,
-  updatePost,
-  getPostByNameAndMessage,
-} from "@/utils/pocketbase";
+	createPost,
+	deletePost,
+	getPosts,
+	updatePost,
+	getPostByNameAndMessage,
+} from "@/utils/pocketbase"
 
 export default async function Home() {
-  let posts = await getPosts().catch((err) => {
-    console.log(err);
-    return [];
-  });
+	let posts = await getPosts().catch((err) => {
+		console.log(err)
+		return []
+	})
 
-  return (
-    <>
-      <Posts
-        data={posts}
-        getPosts={getPosts}
-        createPost={createPost}
-        deletePost={deletePost}
-        updatePost={updatePost}
-        getPostByNameAndMessage={getPostByNameAndMessage}
-      />
-    </>
-  );
+	return (
+		<>
+			<Posts
+				data={posts}
+				getPosts={getPosts}
+				createPost={createPost}
+				deletePost={deletePost}
+				updatePost={updatePost}
+				getPostByNameAndMessage={getPostByNameAndMessage}
+			/>
+		</>
+	)
 }
