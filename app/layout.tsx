e797d9aa8a749b2177db1de2f46f317239375d45
Mark M <chtozamm@gmail.com>
2024-01-02 23:46:18 +0300

    Move header from page.tsx to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Annynotes - The world of fantasy ✨",
@@ -13,7 +15,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="font-['Segoe UI'] mx-auto flex max-w-2xl flex-col items-center px-4 font-sans selection:bg-[#ffb220] selection:text-white md:px-0">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
+
+const Header = () => (
+  <header className="py-8">
+    <Link
+      href="/"
+      className="mx-auto flex w-fit items-center justify-center gap-1.5 font-['Ringbearer'] text-4xl font-bold text-[#ffb220] outline-none focus-visible:rounded-lg focus-visible:ring-2 focus-visible:ring-[#ffb220] focus-visible:ring-offset-4"
+    >
+      Annynotes
+      <Image
+        className="mb-1.5"
+        src="/icons/sparkles.svg"
+        width={34}
+        height={34}
+        alt="✨"
+      />
+    </Link>
+    <p className="text-md max-w-[16em] pt-2 text-center font-['Ringbearer'] text-zinc-400 sm:max-w-full">
+      Share a legend or leave a note for a loved one
+    </p>
+  </header>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
-import Image from "next/image";
 import Posts from "./Posts";
 import ShareForm from "./ShareForm";
-import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className="font-sans mx-auto flex flex-col max-w-2xl font-['Segoe UI'] px-4 md:px-0 items-center">
-      <header className="py-8">
-        <Link
-          href="/"
-          className="flex justify-center gap-1.5 font-['Ringbearer'] text-[#ffb220] text-4xl font-bold items-center"
-        >
-          Annynotes
-          <Image
-            className="mb-1.5"
-            src="/icons/sparkles.svg"
-            width={34}
-            height={34}
-            alt="✨"
-          />
-        </Link>
-        <p className="pt-2 max-w-[16em] sm:max-w-full font-['Ringbearer'] text-center text-zinc-400 text-md">
-          Share a legend or leave a note for a loved one
-        </p>
-      </header>
+    <>
       <ShareForm />
       <Posts />
-    </div>
+    </>
   );
 }
