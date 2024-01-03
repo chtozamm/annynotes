80230b828baf675c488efece2c588e0e5826a2ec
Mark M <chtozamm@gmail.com>
2024-01-03 23:57:44 +0300

    Add deletePost action

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createPost(post: Post) {
   if (!post.author) {
     post.author = "stranger";
@@ -10,6 +12,19 @@ export async function createPost(post: Post) {
     body: JSON.stringify(post),
   });
   if (res.status == 200) {
+    revalidatePath("/");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export async function deletePost(id: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DB_URL}/${id}` as string, {
+    method: "DELETE",
+  });
+  if (res.status == 204) {
+    revalidatePath("/");
     return true;
   } else {
     return false;
