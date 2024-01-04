549d20e7fd3b86cae2a453d8bc1565d43a86d9d5
Mark M <chtozamm@gmail.com>
2024-01-04 22:52:30 +0300

    Add support for optimistic updates

--- app/ShareForm.tsx
+++ app/ShareForm.tsx
@@ -3,38 +3,51 @@
 import { useState, useEffect, useRef } from "react";
 import { createPost } from "./actions";
 import { generateUniqueId } from "./utils";
+import { useSWRConfig } from "swr";
 
 export default function ShareForm() {
+  const { mutate } = useSWRConfig();
   const [isOpen, toggleOpen] = useState(false);
   const passkey = useRef("");
+  const name = useRef("");
+
   useEffect(() => {
     passkey.current = localStorage.getItem("annynotes_passkey") || "";
+    name.current = localStorage.getItem("annynotes_name") || "";
   }, []);
+
   const handleForm = async (data: FormData) => {
+    const inputPasskey = data.get("passkey") as string;
+
     if (
-      data.get("passkey") === process.env.NEXT_PUBLIC_PASSKEY ||
+      inputPasskey === process.env.NEXT_PUBLIC_PASSKEY ||
       passkey.current === process.env.NEXT_PUBLIC_PASSKEY
     ) {
-      if (
-        localStorage.getItem("annynotes_passkey") !==
-        process.env.NEXT_PUBLIC_PASSKEY
-      ) {
+      if (passkey.current !== process.env.NEXT_PUBLIC_PASSKEY) {
         localStorage.setItem(
           "annynotes_passkey",
           process.env.NEXT_PUBLIC_PASSKEY,
         );
       }
-      const newPost = {
+
+      const newPost: Post = {
         id: generateUniqueId(),
-        author: data.get("author"),
-        message: data.get("message"),
-      } as Post;
-      const res = await createPost(newPost);
-      if (res) {
-        const postForm = document.getElementById("postForm") as HTMLFormElement;
-        postForm?.reset();
-      } else {
-        console.log("Couldn't post a note: please, try again");
+        author: (data.get("author") as string).trim(),
+        message: data.get("message") as string,
+      };
+
+      try {
+        await createPost(newPost);
+
+        mutate(
+          `${process.env.NEXT_PUBLIC_DB_URL}?sort=-created&perPage=1000`,
+          (prevData) => [newPost, ...(prevData ?? [])],
+          false,
+        );
+        toggleOpen(false);
+        localStorage.setItem("annynotes_name", newPost.author);
+      } catch (error) {
+        console.log("Error posting a note:", error);
         alert("Couldn't post a note: please, try again");
       }
     } else {
@@ -42,6 +55,7 @@ export default function ShareForm() {
       alert("Incorrect passkey");
     }
   };
+
   return (
     <>
       {isOpen && (
@@ -53,37 +67,49 @@ export default function ShareForm() {
           <input
             type="text"
             name="author"
-            className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 focus:bg-[#fffbf7] focus:shadow-md focus:shadow-[#fffbf7] sm:px-3 sm:py-3"
+            defaultValue={name.current}
+            className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 focus-visible:bg-[#fffbf7] focus-visible:shadow-md focus-visible:shadow-[#fffbf7] sm:px-3 sm:py-3"
             placeholder="Name"
             autoComplete="off"
           />
           <textarea
             name="message"
-            className="w-full max-w-sm resize-none overflow-hidden rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 focus:bg-[#fffbf7] focus:shadow-md focus:shadow-[#fffbf7] sm:px-3 sm:py-3"
+            className="w-full max-w-sm resize-none overflow-hidden rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 focus-visible:bg-[#fffbf7] focus-visible:shadow-md focus-visible:shadow-[#fffbf7] sm:px-3 sm:py-3"
             placeholder="Message"
             autoComplete="off"
             rows={3}
             required
           />
-          <input
-            type="password"
-            name="passkey"
-            className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 focus:bg-[#fffbf7] focus:shadow-md focus:shadow-[#fffbf7] disabled:pointer-events-none disabled:text-zinc-400 sm:px-3 sm:py-3"
-            placeholder="Passkey*"
-            defaultValue={passkey.current}
-            disabled={process.env.NEXT_PUBLIC_PASSKEY === passkey.current}
-            autoComplete="off"
-            required
-          />
+          {passkey.current !== process.env.NEXT_PUBLIC_PASSKEY && (
+            <input
+              type="password"
+              name="passkey"
+              className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 focus-visible:bg-[#fffbf7] focus-visible:shadow-md focus-visible:shadow-[#fffbf7] sm:px-3 sm:py-3"
+              placeholder="Passkey*"
+              defaultValue={passkey.current}
+              disabled={passkey.current === process.env.NEXT_PUBLIC_PASSKEY}
+              autoComplete="off"
+              required
+            />
+          )}
           <div className="flex w-full items-center justify-center">
             <p className="px-2 text-center text-sm text-zinc-400">
-              Passkey is required to summon the owl <br />
-              and bring your note to the board
+              {passkey.current === process.env.NEXT_PUBLIC_PASSKEY ? (
+                <>
+                  The owl remembers you <br />
+                  and is ready to fetch your note
+                </>
+              ) : (
+                <>
+                  A passkey is required to summon the owl <br />
+                  and have it bring your note to the board
+                </>
+              )}
             </p>
             <span className="text-3xl">🦉</span>
           </div>
           <button
-            className="mx-auto mb-4 w-full max-w-sm rounded-xl bg-[#ffb220] py-4 text-[0.75em] font-black uppercase text-white outline-none focus:ring-2 focus:ring-[#ffb220] focus:ring-offset-4"
+            className="mx-auto mb-4 w-full max-w-sm rounded-xl bg-[#ffb220] py-4 text-[0.75em] font-black uppercase text-white outline-none focus-visible:ring-2 focus-visible:ring-[#ffb220] focus-visible:ring-offset-4"
             type="submit"
           >
             Post
@@ -91,7 +117,7 @@ export default function ShareForm() {
         </form>
       )}
       <button
-        className={`mx-auto w-full max-w-sm rounded-xl py-4 text-[0.75em] font-black uppercase outline-none focus:ring-2 focus:ring-[#ffb220] focus:ring-offset-4
+        className={`mx-auto w-full max-w-sm rounded-xl py-4 text-[0.75em] font-black uppercase outline-none focus-visible:ring-2 focus-visible:ring-[#ffb220] focus-visible:ring-offset-4
       ${
         isOpen
           ? "border-2 border-[#ffb220] bg-white text-[#ffb220]"
