3e011e84906fb82d603bbf908c626b5d4ad582a7
Mark M <chtozamm@gmail.com>
2024-01-08 23:26:33 +0300

    Style: add margin bottom to form to prevent touching end of screen

--- app/posts/new/ShareForm.tsx
+++ app/posts/new/ShareForm.tsx
@@ -31,41 +31,39 @@ export default function ShareForm() {
   };
 
   return (
-    <>
-      <form
-        action={handleForm}
-        className="flex w-full max-w-sm flex-col items-center gap-4"
-        id="postForm"
-      >
-        <input
-          type="text"
-          name="author"
-          className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 sm:px-3 sm:py-3 lg:focus-visible:bg-[#fffbf7] lg:focus-visible:shadow-md lg:focus-visible:shadow-[#fffbf7]"
-          placeholder="Name"
-          autoComplete="off"
-        />
-        <TextArea />
-        <input
-          type="password"
-          name="passkey"
-          className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 sm:px-3 sm:py-3 lg:focus-visible:bg-[#fffbf7] lg:focus-visible:shadow-md lg:focus-visible:shadow-[#fffbf7]"
-          placeholder="Passkey*"
-          autoComplete="off"
-          required
-        />
-        <div className="flex w-full items-center justify-center">
-          <p className="px-2 text-center text-sm text-zinc-400">
-            {/* <>
+    <form
+      action={handleForm}
+      className="mb-8 flex w-full max-w-sm flex-col items-center gap-4"
+      id="postForm"
+    >
+      <input
+        type="text"
+        name="author"
+        className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 sm:px-3 sm:py-3 lg:focus-visible:bg-[#fffbf7] lg:focus-visible:shadow-md lg:focus-visible:shadow-[#fffbf7]"
+        placeholder="Name"
+        autoComplete="off"
+      />
+      <TextArea />
+      <input
+        type="password"
+        name="passkey"
+        className="w-full max-w-sm rounded-xl border-t-2 border-[#ffb220] px-2 py-2 outline-none placeholder:text-sm placeholder:text-zinc-400 sm:px-3 sm:py-3 lg:focus-visible:bg-[#fffbf7] lg:focus-visible:shadow-md lg:focus-visible:shadow-[#fffbf7]"
+        placeholder="Passkey*"
+        autoComplete="off"
+        required
+      />
+      <div className="flex w-full items-center justify-center">
+        <p className="px-2 text-center text-sm text-zinc-400">
+          {/* <>
                 The owl remembers you <br />
                 and is ready to fetch your note
               </> */}
-            A passkey is required to summon the owl <br />
-            and have it bring your note to the board
-          </p>
-          <span className="text-3xl">🦉</span>
-        </div>
-        <SubmitButton innerText="Post" />
-      </form>
-    </>
+          A passkey is required to summon the owl <br />
+          and have it bring your note to the board
+        </p>
+        <span className="text-3xl">🦉</span>
+      </div>
+      <SubmitButton innerText="Post" />
+    </form>
   );
 }
