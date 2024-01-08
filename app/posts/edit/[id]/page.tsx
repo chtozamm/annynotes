d2a31cb244bfdf20f9cbeae08b2d94cc6141c9c9
Mark M <chtozamm@gmail.com>
2024-01-08 18:14:23 +0300

    Handle the case where post with the id does not exist

--- app/posts/edit/[id]/page.tsx
+++ app/posts/edit/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { validateId } from "@/app/utils";
 import UpdateForm from "./UpdateForm";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export default async function Page({ params }: { params: { id: string } }) {
   if (validateId(params.id)) {
@@ -7,6 +9,11 @@ export default async function Page({ params }: { params: { id: string } }) {
       process.env.NEXT_PUBLIC_DB_URL + "/" + params.id,
       { cache: "no-store" },
     ).then((res) => res.json());
+
+    if (post.message === "The requested resource wasn't found.") {
+      revalidatePath("/");
+      redirect("/");
+    }
 
     return <UpdateForm post={post} />;
   } else {
