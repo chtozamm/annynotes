93f4261b6f8d2ec0f819f231f02207dcadf28be5
Mark M <chtozamm@gmail.com>
2024-04-07 09:29:40 +0300

    Create middleware to protect routes

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest) {
+  if (request.cookies.has("token")) return NextResponse.next()
+  if (
+    request.cookies.has("token") &&
+    (request.url === "/signin" || request.url === "/signup")
+  )
+    return NextResponse.redirect(new URL("/", request.url))
+  return NextResponse.redirect(new URL("/signin", request.url))
+}
+
+export const config = {
+  matcher: ["/profile/:path*", "/posts/new"],
+}
