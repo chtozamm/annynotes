331b81be45c5b0763e5f2208f068c8eafd8faf27
Mark M <chtozamm@gmail.com>
2024-05-08 21:01:42 +0300

    Cleanup

--- app/profile/posts/page.tsx
+++ app/profile/posts/page.tsx
@@ -3,17 +3,18 @@ import LinkButton from "@/components/LinkButton";
 import Posts from "@/components/Posts";
 
 export default async function UserPosts() {
-  const data: Post[] = await fetch(
+  const [_, userId, __] = await getSession();
+  const posts: Post[] = await fetch(
     process.env.NEXT_PUBLIC_DB_URL + "?sort=-created&perPage=1000",
     { next: { tags: ["posts"] }, cache: "force-cache" },
   )
     .then((res) => res.json())
-    .then((data) => data.items);
-  const [_, userId, __] = await getSession();
-  const posts = data.filter((post) => post.user_id === userId);
+    .then((data) => (data.items as Post[])
+    .filter((post) => post.user_id === userId)
+  );
   return (
     <>
-      {/* <LinkButton label="Share" /> */}
+      <LinkButton label="Share" />
       <h2 className="mb-8 w-full text-center font-ringbearer text-2xl font-bold lowercase text-primary">
         {posts.length > 0 ? "Your notes:" : "You haven't posted anything yet"}
       </h2>
